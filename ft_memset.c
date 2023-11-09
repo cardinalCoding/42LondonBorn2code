913587b59e48777a5f650140c6833f576239a40f
cardinalCoding <cardinalequilibrium@gmail.com>
2023-11-09 07:21:54 +0000

    changes
    Added Changes to files

--- ft_memset.c
+++ ft_memset.c
@@ -6,23 +6,23 @@
 /*   By: bcorte-r <bcorte-r@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 10:55:46 by bcorte-r          #+#    #+#             */
-/*   Updated: 2023/11/03 06:26:55 by bcorte-r         ###   ########.fr       */
+/*   Updated: 2023/11/07 10:45:51 by bcorte-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t			i;
 	unsigned char	*p;
 
 	i = 0;
-	p = (unsigned char *)b;
-	while (len > i)
+	p = (unsigned char *)s;
+	while (n > i)
 	{
 		p[i] = c;
 		i++;
 	}
-	return (b);
+	return (s);
 }
--- libft.h
+++ libft.h
@@ -37,6 +37,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strchr(const char *s, int i);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
