3c97f516cf9d57aeb2a121b7a7da33987e6b4c18
cardinalCoding <cardinalequilibrium@gmail.com>
2023-11-10 11:22:29 +0000

    Update ft_strmapi.c
    added strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,28 @@
 /*   By: bcorte-r <bcorte-r@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 23:20:13 by bcorte-r          #+#    #+#             */
-/*   Updated: 2023/11/07 06:20:28 by bcorte-r         ###   ########.fr       */
+/*   Updated: 2023/11/10 10:35:02 by bcorte-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strnmapi(char const *s, char (f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
